55edfa2c8743964fdd6a1882d9ee8681ede24414
phantie <funtom1605@gmail.com>
2024-06-04 14:36:10 +0000

    bump version number

--- ruption/__init__.py
+++ ruption/__init__.py
@@ -1,24 +1,4 @@
-"""
-- Implements pacify module
-- Implements subset of [Option](https://doc.rust-lang.org/std/option/enum.Option.html) enum 
-- Implements subset of [Result](https://doc.rust-lang.org/std/result/enum.Result.html) enum
-
-Imports:
-
-    from ruption.pacify import *
-    from ruption.option import Option, some, none
-    from ruption.result import Result, ok, err
-    from ruption.panic import Panic
-
-    
-Installation:
-    
-    pip install git+https://github.com/phantie/ruption.git
-
-"""
-
-
 
 __all__ = ["__version__"]
-__version__ = 1, 4, 1
 
+__version__ = 2, 0, 0
