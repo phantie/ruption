fa805cc958cdc8f330b506a8aa5bc537edca99b6
phantie <funtom1605@gmail.com>
2024-07-04 11:54:02 +0000

    feat: good type typehinting after decorator application

--- ruption/pacify.py
+++ ruption/pacify.py
@@ -1,6 +1,6 @@
 
 
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, ParamSpec
 import functools
 from .option import some, none, Option
 from .result import ok, err, Result, Err
@@ -15,6 +15,7 @@ __all__ = [
 
 
 R = TypeVar('R')
+P = ParamSpec("P")
 
 def log_fn(e) -> None:
     global logger
@@ -40,15 +41,15 @@ def pacify_call(fn: Callable[[], R], *, log = True, log_fn = log_fn) -> Option[R
 def pacify_callable(
     log: bool = True,
     log_fn: Callable[[Exception], None] = log_fn) \
-    -> Callable[[Callable[..., R]], Callable[..., Option[R]]]:
+    -> Callable[[Callable[P, R]], Callable[P, Option[R]]]:
 
-    def pacify_callable(func: Callable[..., R]) -> Callable[..., Option[R]]:
+    def pacify_callable(func: Callable[P, R]) -> Callable[P, Option[R]]:
         """Protects function call from any exception popping,
         On success returns function result wrapped in some
         On exception returns none"""
 
         @functools.wraps(func)
-        def wrapper(*args, **kwargs) -> Option[R]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> Option[R]:
             return pacify_call(lambda: func(*args, **kwargs), log = log, log_fn = log_fn)
         return wrapper
     return pacify_callable
@@ -65,15 +66,15 @@ def pacify_call_result(fn: Callable[[], R]) -> Result[R, Err]:
 
 
 def pacify_callable_result() \
-    -> Callable[[Callable[..., R]], Callable[..., Result[R, Err]]]:
+    -> Callable[[Callable[P, R]], Callable[P, Result[R, Err]]]:
 
-    def pacify_callable_result(func: Callable[..., R]) -> Callable[..., Result[R, Err]]:
+    def pacify_callable_result(func: Callable[P, R]) -> Callable[P, Result[R, Err]]:
         """Protects function call from any exception popping,
-        On success returns function result wrapped in ok
+        On success returns fun/ction result wrapped in ok
         On exception returns error wrapped in err"""
 
         @functools.wraps(func)
-        def wrapper(*args, **kwargs) -> Result[R, Err]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> Result[R, Err]:
             return pacify_call_result(lambda: func(*args, **kwargs))
         return wrapper
     return pacify_callable_result
